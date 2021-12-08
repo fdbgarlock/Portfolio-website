8b295d11c5c47d9acb248f4242f58562579fb600
Faith Garlock <faith.garlock@porttechnologyservices.com>
2021-12-08 09:11:06 -0500

    validator corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Welcome to my page!");
+console.log('Welcome to my page!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
